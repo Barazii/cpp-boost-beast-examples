8460e53080e563f89bc89aea25618adeb8154ec5
Barazii <mahmoud.baraziii@gmail.com>
2025-05-15 15:35:27 +0200

    Refactored the client to be similar to the server - have two threads one for reading and one for writing and use std::string instead of char[] array.

--- client-server-messaging-app/client.cpp
+++ client-server-messaging-app/client.cpp
@@ -6,44 +6,37 @@ using boost::asio::ip::tcp;
 
 void read_messages(tcp::socket &socket)
 {
-    char data[1024];
+    std::string data(1024, '\0');
     while (true)
     {
-        std::memset(data, 0, sizeof(data));
-        boost::system::error_code error;
-        socket.read_some(boost::asio::buffer(data), error);
-        if (error)
-            break;
-        std::cout << "Server: " << data << std::endl;
+        if (socket.read_some(boost::asio::buffer(data, data.max_size())))
+        {
+            std::cout << "Server: " << data << std::endl;
+            data.clear();
+            data.resize(1024);
+        }
     }
 }
 
-void chat_client(const std::string &server_ip)
+void write_messages(tcp::socket &socket)
 {
-    try
+    std::string message;
+    while (true)
     {
-        boost::asio::io_context io_context;
-        tcp::socket socket(io_context);
-        socket.connect(tcp::endpoint(boost::asio::ip::make_address(server_ip), 12345));
-
-        // Start reader thread
-        std::thread reader_thread(read_messages, std::ref(socket));
-
-        // Main thread handles writing
-        while (true)
-        {
-            std::string message;
-            std::cout << "You: ";
-            std::getline(std::cin, message);
-            boost::asio::write(socket, boost::asio::buffer(message));
-        }
-
-        reader_thread.join();
+        message.clear();
+        std::cout << "You: ";
+        std::getline(std::cin, message);
+        boost::asio::write(socket, boost::asio::buffer(message, message.max_size()));
     }
-    catch (std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+}
+
+void communicate(tcp::socket && socket)
+{
+    std::thread reader_thread{read_messages, std::ref(socket)};
+    std::thread writer_thread{write_messages, std::ref(socket)};
+
+    reader_thread.join();
+    writer_thread.join();
 }
 
 int main()
@@ -52,6 +45,10 @@ int main()
     std::cout << "Enter server IP: ";
     std::cin >> server_ip;
     std::cin.ignore(); // Ignore leftover newline from std::cin
-    chat_client(server_ip);
-    return 0;
+
+    boost::asio::io_context io_context;
+    tcp::socket socket(io_context);
+    socket.connect(tcp::endpoint(boost::asio::ip::make_address(server_ip), 12345));
+
+    communicate(std::move(socket));
 }
